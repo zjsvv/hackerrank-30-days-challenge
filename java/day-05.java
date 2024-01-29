79576b431eb84c6740b68cc4a8942605cc977d0c
zjs <jinsheng358@gmail.com>
2024-01-29 13:29:41 -0800

    day05

---
+++ java/day-05.java
@@ -0,0 +1,29 @@
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+
+public class Solution {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader bufferedReader = new BufferedReader(
+      new InputStreamReader(System.in)
+    );
+
+    int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+    bufferedReader.close();
+
+    for (var i = 1; i <= 10; i++) {
+      System.out.printf("%d x %d = %d\n", n, i, n * i);
+    }
+  }
+}
